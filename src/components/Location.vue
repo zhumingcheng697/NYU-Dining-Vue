052d144b120220202c02eb952d2859fc50dc739a
McCoy Zhu <mz1945@nyu.edu>
2021-03-03 22:15:19 -0800

    UPDATE: more schedule data reformat

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -2,11 +2,13 @@
   <div class="Location">
     <h2>{{ locationInfo["name"] || "Unknown Location" }}</h2>
     <p>{{ address || "No Address Information" }}</p>
-    <pre v-for="(schedule, key) in schedules" :key="key">{{schedule}}</pre>
+    <pre v-for="([days, schedule], key) in schedules" :key="key">{{ days }} {{ schedule }}</pre>
   </div>
 </template>
 
 <script>
+// const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 export default {
   name: "Location",
   props: {
@@ -18,7 +20,28 @@ export default {
       return [street, city, state].filter(Boolean).join(", ") + (zip_code ? ` ${zip_code}` : "");
     },
     schedules() {
-      return (this.locationInfo["schedules"] || []).filter((el) => el["type"] === "location");
+      return Object.entries((this.locationInfo["schedules"] || []).filter((el) => {
+        return el["type"] === "location" && el["days"] && Array.isArray(el["days"]) && el["start_hour"] && el["end_hour"];
+      }).reduce((prev, curr) => {
+        const dayKey = curr["days"].sort((a, b) => {
+          return (a === 0 ? 7 : a) - (b === 0 ? 7 : b);
+        }).join(",");
+
+        if (!prev[dayKey]) {
+          prev[dayKey] = [];
+        }
+
+        prev[dayKey].push({ start: curr["start_hour"], end: curr["end_hour"] });
+        return prev;
+      }, {})).sort((a, b) => {
+        if (a[0] < b[0]) {
+          return -1;
+        } else if (a[0] > b[0]) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
     }
   }
 };
