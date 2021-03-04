25c04c27ad5abef46bbe15a03b3e27372964429c
McCoy Zhu <mz1945@nyu.edu>
2021-03-03 22:21:03 -0800

    FIX: sunday comes last

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -20,23 +20,25 @@ export default {
       return [street, city, state].filter(Boolean).join(", ") + (zip_code ? ` ${zip_code}` : "");
     },
     schedules() {
-      return Object.entries((this.locationInfo["schedules"] || []).filter((el) => {
-        return el["type"] === "location" && el["days"] && Array.isArray(el["days"]) && el["start_hour"] && el["end_hour"];
-      }).reduce((prev, curr) => {
-        const dayKey = curr["days"].sort((a, b) => {
-          return (a === 0 ? 7 : a) - (b === 0 ? 7 : b);
-        }).join(",");
+      return Object.entries(
+          (this.locationInfo["schedules"] || []).filter((el) => {
+            return el["type"] === "location" && el["days"] && Array.isArray(el["days"]) && el["start_hour"] && el["end_hour"];
+          }).reduce((prev, curr) => {
+            const dayKey = curr["days"].sort((a, b) => {
+              return ((a === 0 ? 7 : a) - (b === 0 ? 7 : b));
+            }).join(",");
 
-        if (!prev[dayKey]) {
-          prev[dayKey] = [];
-        }
+            if (!prev[dayKey]) {
+              prev[dayKey] = [];
+            }
 
-        prev[dayKey].push({ start: curr["start_hour"], end: curr["end_hour"] });
-        return prev;
-      }, {})).sort((a, b) => {
-        if (a[0] < b[0]) {
+            prev[dayKey].push({ start: curr["start_hour"], end: curr["end_hour"] });
+            return prev;
+          }, {})
+      ).sort((a, b) => {
+        if ((a[0] === "0" ? "7" : a[0]) < (b[0] === "0" ? "7" : b[0])) {
           return -1;
-        } else if (a[0] > b[0]) {
+        } else if ((a[0] === "0" ? "7" : a[0]) > (b[0] === "0" ? "7" : b[0])) {
           return 1;
         } else {
           return 0;
